ed2842901c9b562e7233f0785949d3976996ed81
Nguyen Van Hoang Phuc <hoangphucnguyenvan1@gmail.com>
2024-09-21 15:00:38 +0700

    Add project files.

---
+++ Lab2/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN_Lab2
+{
+    internal class Product
+    {
+        private string p_name;
+        private double p_price;
+        private int p_discount;
+        public Product() { }
+        public Product(string p_name, double p_price, int p_discount)
+        {
+            this.p_name = p_name;
+            this.p_price = p_price;
+            this.p_discount = p_discount;
+        }
+
+        public Product(string p_name, double p_price)
+        {
+            this.p_name = p_name;
+            this.p_price = p_price;
+            this.p_discount = 0;
+        }
+        public string Product_Name { get; set; }
+        public double Product_Price { get; set; }
+        public int Discount { get; set; }
+
+        public void input()
+        {
+            Console.WriteLine("Please enter product's name : ");
+            this.p_name = Console.ReadLine();
+            Console.WriteLine("Please enter product's price : ");
+            this.p_price = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter product's discount :");
+            this.p_discount = int.Parse(Console.ReadLine());
+        }
+
+        private double getImportTax()
+        {
+            return (p_price * 10) / 100;
+        }
+
+        public void display()
+        {
+            Console.WriteLine($"Product name : {p_name}");
+            Console.WriteLine($"Product price : {p_price}");
+            Console.WriteLine($"Product discount : {p_discount}");
+            Console.WriteLine($"Product tax : {getImportTax()}");
+        }
+
+    }
+}
---
+++ Lab2/main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN_Lab2
+{
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            /*2.
+            Product product1 = new Product();
+            Product product2 = new Product();
+            product1.input();
+            product2.input();
+            product1.display();
+            product2.display(); */
+
+
+            /*3.
+            Product product1 = new Product("iphone",800000,10);
+            Product product2 = new Product("android",500000);
+            product1.display();
+            product2.display();*/
+
+
+        }
+    }
+}
